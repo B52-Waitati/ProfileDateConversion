0e8392f6ed2b1bb737fa08987b7ec72a258e169e
Worik <5387413+worikgh@users.noreply.github.com>
2020-09-10 10:52:53 +1200

    Initial Commit

---
+++ ProfileDateConversion/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProfileDateConversion
+//
+//  Created by Worik on 10/09/20.
+//  Copyright © 2020 B52-Waitati. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
